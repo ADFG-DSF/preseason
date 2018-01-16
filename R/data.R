08d8fe85d3622ad86631ef07da5832b22c29e6e3
Adam <adam.reimer@alaska.gov>
2018-01-16 14:25:07 -0900

    Data documentation for Deshka brood table

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Deshka River Chinook salmon brood table
+#'
+#' Updated thru the 2017 season
+#'
+#' @format A data frame with 44 rows and 7 variables:
+#' \describe{
+#'   \item{bry}{brood year}
+#'   \item{S}{Spawners during each brood year}
+#'   \item{age3}{Age 3 fish returning from each brood year}
+#'   \item{age4}{Age 4 fish returning from each brood year}
+#'   \item{age5}{Age 5 fish returning from each brood year}
+#'   \item{age6}{Age 6 fish returning from each brood year}
+#'   \item{age7}{Age 7 fish returning from each brood year}
+#' }
+"deshka"
+   
