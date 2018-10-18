c466915befc4843124808ff47f1b0bb53da314c9
Reimer <adam.reimer@dfg.alaska.local>
2018-10-18 13:44:25 -0800

    Use SusitnaEG posterior for brood table

--- data_raw/dat_brood.R
+++ data_raw/dat_brood.R
@@ -1,14 +1,24 @@
-brood0 <-
-readxl::read_excel("H:\\My Documents\\Deshka R, Chinook and Coho\\Forecast\\preseason\\data_raw\\Copy of Deshka KS Brood Master.xlsx",
-                   range = "Master!CH18:CM61",
-                   col_names = c("byr", "age3", "age4", "age5", "age6", "age7")) %>%
-  dplyr::mutate_all(round)
+# Create a Deshka Brood table based on the most recent SusitnaEG posterior. Should update prior to forecast.
+# N.tas, or abundance by calendar year, age and stock is required but was not monitored, calculated here instead.
+library(magrittr)
+post <- readRDS("H:\\My Documents\\SusitnaEG\\posts\\SuChinook_DsumS_ababd3a.rds")
+Y <- 39
+A <- 4
+N.tas.sim <- array(NA, dim = c(2250, 39, 4, 5))
+for (sim in 1:2250){
+  for (stock in 1:5){
+    for (a in 1:A) {
+      for (c in a:(Y + (a - 1))) {
+        N.tas.sim[sim, c - (a - 1), (A + 1 - a), stock] <- post$sims.list$p[sim, c, (A + 1 - a)] * post$sims.list$R[sim, c, stock]
+      }
+    }
+  }
+}
 
-spawn <-
-  readxl::read_excel("H:\\My Documents\\Deshka R, Chinook and Coho\\Forecast\\preseason\\data_raw\\Copy of Deshka KS Brood Master.xlsx",
-                     range = "Master!CX18:Cy61",
-                     col_names = c("byr", "S")) %>%
-  dplyr::mutate_all(round)
-
-deshka <- dplyr::left_join(spawn, brood0, by = "byr") %>% print(n = 100)
+post$mean$N.tas <- apply(N.tas.sim, c(2, 3, 4), mean)
+year_id <- as.character(1979:2017)
+deshka <- 
+  SusitnaEG::table_brood(post, 1) %>%
+  dplyr::filter(year >= 1979) %>%
+  dplyr::select(byr = year, S, dplyr::starts_with("age"))
 devtools::use_data(deshka, overwrite = TRUE)
