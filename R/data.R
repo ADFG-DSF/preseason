ceeea15c7bcf94ca8d95f17f63281eb6cbafadca
Reimer <adam.reimer@dfg.alaska.local>
2018-10-18 16:11:32 -0800

    2018 added to brood table from DeshkaWeir pack

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Deshka River Chinook salmon brood table
 #'
-#' Based on SusitnEG posterior updated thru the 2017 season (Chinook_DsumS_ababd3a.rds)
+#' Based on SusitnEG posterior updated thru the 2017 season (Chinook_DsumS_ababd3a.rds), but temporarily appended w 2018 data from Deshka asl.
 #'
 #' @format A data frame with 44 rows and 7 variables:
 #' \describe{
@@ -10,7 +10,6 @@
 #'   \item{age4}{Age 4 fish returning from each brood year}
 #'   \item{age5}{Age 5 fish returning from each brood year}
 #'   \item{age6}{Age 6 fish returning from each brood year}
-#'   \item{age7}{Age 7 fish returning from each brood year}
 #' }
 "deshka"
    
