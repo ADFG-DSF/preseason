1e83fbe7ff414b8807eeea3e3290821422a13770
Reimer <adam.reimer@dfg.alaska.local>
2018-11-01 16:12:03 -0800

    Added natural scale medians to pred_ma

--- R/pred_ma.R
+++ R/pred_ma.R
@@ -2,18 +2,27 @@
 #'
 #' One step forward predictions from a moving average model
 #'
-#' @param mod a class "lm" object
+#' @param var a log transformed time series. 
+#' @param yrs years in the moving average. NULL to average the entire series.
 #'
-#' @return a vector of predictions.
+#' @return a matrix with columns "mean" and "median" providing predictions on the natural scale.
 #'
 #' @examples
-#' pred_lm(mod)
+#' pred_ma(var)
 #'
 #' @export
-pred_ma <- function(var, yrs = NULL){
-  sapply(1:length(var), function(x){
-    if(is.null(yrs)) {start <- 1} else {start <- max(1, x-yrs)}
-    y <- var[start:(x-1)]
-    exp(mean(y) + var(y)/2)
-  })
+pred_ma <- function(var, yrs = 5){
+  df <-
+    lapply(1:length(var), function(x) {
+      if (is.null(yrs)) {
+        start <- 1
+      }
+      else {
+        start <- max(1, x - yrs)
+      }
+      y <- var[start:(x - 1)]
+      c(mean = exp(mean(y) + var(y)/2),
+        median = exp(mean(y)))
+    })
+  do.call(rbind, df)
 }
