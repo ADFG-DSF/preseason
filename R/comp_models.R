4da9ed7bae02e8804d918bf5eda3c62b01374459
Adam Reimer <adam.reimer@alaska.gov>
2024-12-16 10:57:29 -0900

    Update comp_models to include maape
    Also fix so depricated dplyr functionality

--- R/comp_models.R
+++ R/comp_models.R
@@ -8,7 +8,7 @@
 #'  \item{xxx_pred}{one or more columns containing the hindcast prediction for the return of fish of "comp_age" using the naming convention xxx_pred. The prefix "xxx" will be used to name the prediction in the output.}}
 #' @param comp_age The total age of fish being forecast.
 #' @param years The number of past years used to evaluate forecast accuracy.
-#' @param metric The performance metrics used to evaluate forecast accuracy; "md" = "mean deviation", "mad" = "mean absolute deviation" or "mape" = "mean absolute percent error".
+#' @param metric The performance metrics used to evaluate forecast accuracy; "md" = "mean deviation", "mad" = "mean absolute deviation", "mape" = "mean absolute percent error", "maape" = "mean absolute arctan percent error".
 #'
 #' @return A list containing one figure and one table.
 #'
@@ -20,37 +20,33 @@
 #' comp_models(dat = dat6, comp_age = 6, years = 5)
 #'
 #' @export
-comp_models <- function(dat, comp_age, years = 5, metric = c("md", "mad")){
-  stopifnot(sum(metric %in% c("mape", "md", "mad")) == length(metric))
-  
+comp_models <- function (dat, comp_age, years = 5, metric = c("md", "mad", "maape")) 
+{
+  stopifnot(sum(metric %in% c("maape", "mape", "md", "mad")) == length(metric))
   age <- paste0("age", comp_age)
-  diff <- dat %>%
-    dplyr::select(byr, !!age, dplyr::ends_with("pred")) %>%
-    tidyr::gather(type, pred, -byr, -!!age) %>%
-    dplyr::mutate(d = pred - (!!as.name(age)),
-                  pe = d / (!!as.name(age)))
-
-plot <- ggplot2::ggplot(diff, ggplot2::aes(x = byr, y = pred, color = type)) +
-  ggplot2::geom_jitter(width = 0.2, alpha = 0.5, size = 3) +
-  ggplot2::geom_point(ggplot2::aes_string(y = age), color = "black") +
-  ggplot2::geom_line(ggplot2::aes_string(y = age), color = "black")
-
-table0 <- diff %>%
-  dplyr::group_by(type) %>%
-  dplyr::top_n(years, byr)
-
-  if("mape" %in% metric){
-    table <- 
-      table0 %>%
-      dplyr::summarise(md = mean(d),
-                       mad = mean(abs(d)),
-                       mape = mean(abs(pe)))
-  }else{
-    table <- 
-      table0 %>%
-      dplyr::summarise(md = mean(d),
-                       mad = mean(abs(d)))
-  }
-
-list(plot, table)
+  
+  diff <- 
+    dat %>% 
+    dplyr::select(byr, !!age, dplyr::ends_with("pred")) %>% 
+    tidyr::gather(type, pred, -byr, -!!age) %>% 
+    dplyr::mutate(d = pred - (!!as.name(age)), 
+                  pe = d/(!!as.name(age)))
+  
+  plot <- 
+    ggplot2::ggplot(diff, ggplot2::aes(x = byr, y = pred, color = type)) + 
+    ggplot2::geom_jitter(width = 0.2, alpha = 0.5, size = 3) + 
+    ggplot2::geom_point(ggplot2::aes(y = .data[[age]]), color = "black") + 
+    ggplot2::geom_line(ggplot2::aes(y = .data[[age]]), color = "black")
+  
+  table <- 
+    diff %>% 
+    dplyr::group_by(type) %>% 
+    dplyr::top_n(years, byr) %>% 
+    dplyr::summarise(md = mean(d), 
+                     mad = mean(abs(d)), 
+                     mape = mean(abs(pe)),
+                     maape = mean(atan(abs(pe)))) %>%
+    dplyr::select(-c("maape", "mape", "md", "mad")[!(c("maape", "mape", "md", "mad") %in% metric)])
+  
+  list(plot = plot, table = table)
 }
