742e8bc43a8012da139e191e00bc26aae2ccab93
Adam Reimer <adam.reimer@alaska.gov>
2024-12-19 12:05:13 -0900

    Allow user to specify an ets model during hindcasts

--- R/pred_es.R
+++ R/pred_es.R
@@ -3,6 +3,7 @@
 #' Creates a series of one-step ahead predictions for all years beyond the 5th year in the time series using the an exponential smoothing ("ANN") model to be used in evaluating model performance in past seasons.
 #'
 #' @param x a time series.
+#' @param model_0 A three-character string identifying model arg for forecast::ets. The first letter denotes the error type ("A", "M" or "Z"); the second letter denotes the trend type ("N","A","M" or "Z"); and the third letter denotes the season type ("N","A","M" or "Z"). In all cases, "N"=none, "A"=additive, "M"=multiplicative and "Z"=automatically selected. In this usage "ANN" (the default) is general selected by forecast::ets but if the user notices a different specification this parameters allows them to specify that model during the hindcast.
 #'
 #' @return a vector of predictions.
 #'
@@ -11,10 +12,10 @@
 #' pred_es(dat6$age6_ln)
 #'
 #' @export
-pred_es <- function(x){
+pred_es <- function(x, model_0 = "ANN"){
   preds <- sapply(6:length(x), function(l){
     train <- x[1:(l-1)]
-    newmod <- forecast::ets(train, "ANN")
+    newmod <- forecast::ets(train, model = model_0)
     predict(newmod, h = 1)$mean})
   c(rep(NA, 5), exp(preds))
 }
